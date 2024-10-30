59792d5fd051a82337384da0384824a7155e15c3
Ronney Rocha <ronneyrv@gmail.com>
2024-10-30 17:35:30 -0300

    Alteração da cor dos botões

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -31,7 +31,7 @@ function AddTask({ onAddTaskSubmit }) {
           setTitle("");
           setDescripition("");
         }}
-        className="bg-slate-400 text-black px-4 py-2 rounded-md font-bold"
+        className="bg-slate-400 text-white px-4 py-2 rounded-md font-bold"
       >
         Adicionar
       </button>
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
 function Button(props) {
   return (
-    <button {...props} className="bg-slate-400 p-2 rounded-md text-black">
+    <button {...props} className="bg-slate-400 p-2 rounded-md text-white">
       {props.children}
     </button>
   );
